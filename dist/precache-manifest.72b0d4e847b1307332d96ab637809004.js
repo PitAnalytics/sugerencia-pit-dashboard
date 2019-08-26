ed85147fe88f2b67c0339d04ac842070d8cc277e
HardCoreWare <bernardocodu@gmail.com>
2019-08-26 14:51:06 -0500

    ok

---
+++ dist/precache-manifest.72b0d4e847b1307332d96ab637809004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "63f6d00a34a70a8e674e5374b1ab00e9",
+    "url": "/styles.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1ee5ec6fba837d82107b",
+    "url": "/js/chunk-vendors.adc5ee78.js"
+  },
+  {
+    "revision": "71e230f6da10b855c001",
+    "url": "/js/app.c6d3034c.js"
+  },
+  {
+    "revision": "b097243cb9119bf4ab49f1f1b3fbb884",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71e230f6da10b855c001",
+    "url": "/css/app.41dd3f12.css"
+  }
+];
